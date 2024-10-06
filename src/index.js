a8d92e354cbb80e735f7690e32197cf60fc51434
Ishmeet Singh Saini <singhishmeetsaini@gmail.com>
2024-10-06 21:05:43 +0530

    added express config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(() =>{
+    app.on("error",(error) => {
+        console.log("ERROR: ", error);
+        throw error   
+    })
+})
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connetion FAILED!!", err);
+})
+
+app.get("/" ,(req,res) => {
+    res.send("hello")
+})
