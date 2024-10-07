676b3d325a9a8afa48eebaa4fdc2407f774fa99e
Ishmeet Singh Saini <singhishmeetsaini@gmail.com>
2024-10-07 21:22:44 +0530

    h

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//via try catch
+
+// const asyncHandler = (requestHandler) => async(req,res,next) => {
+//     try {
+//         await requestHandler(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+//via promises
+
+const asyncHandler = (requestHandeler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandeler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
